f3a5081e0ff34873d70cf9ca11ef3bba78feef2c
Olivier Latignies <ol@two4tek.eu>
2024-01-30 14:24:37 +0100

    Create new Vaccination

--- query.py
+++ query.py
@@ -1,17 +1,47 @@
 ''' Query tool for RWS FHIR services '''
 import rswfhir
 import json
+import uuid
 from fhir.resources.R4B.bundle import Bundle
 from fhir.resources.BE.STU1.patient import BePatient
+from fhir.resources.BE.STU1.vaccination import BeVaccination
+from fhir.resources.R4B.reference import Reference
+from fhir.resources.R4B.immunization import Immunization
+
+PATIENT_SSIN = '69062100976'
+DOCTOR_NIDHI = '10840046004'
+DOCTOR_SSIN = '69062100976'
+SYSTEM_SSIN = 'https://www.ehealth.fgov.be/standards/fhir/core/NamingSystem/ssin'
 
 # load bearer
 BEARER = ''
 with open('bearer.txt') as f:
     BEARER = f.read()
     print(BEARER)
+# load vaccination
+VACCINE_JSON = ''
+with open('success.json') as f:
+    VACCINE_JSON = f.read()
+    print(VACCINE_JSON)
+
 RSW = rswfhir.rswfhir(BEARER)
-result = RSW.call('Patient/_search',{'patient.identifier':'https://www.ehealth.fgov.be/standards/fhir/core/NamingSystem/ssin|69062100976'})
-result_json = result.json()
-bundle = Bundle(**result_json)
-patient = bundle.entry[0].resource
-print (json.dumps(patient.dict(), default=str))
+# request patient
+result = RSW.call('Patient/_search',{'patient.identifier':'https://www.ehealth.fgov.be/standards/fhir/core/NamingSystem/ssin|' + PATIENT_SSIN})
+print (json.dumps(result.dict(), default=str))
+
+vaccine_data = json.loads(VACCINE_JSON)
+vaccine = BeVaccination(**vaccine_data)
+vaccine.identifier[0].value = str(uuid.uuid4())
+vaccine.patient.identifier.value = PATIENT_SSIN
+vaccine.patient.identifier.system = SYSTEM_SSIN
+vaccine.performer[0].actor.identifier.value = DOCTOR_SSIN
+vaccine.performer[0].actor.identifier.system = SYSTEM_SSIN
+print (vaccine.resource_type == "BeVaccination")
+print (isinstance(vaccine.patient, Reference))
+print (json.dumps(vaccine.dict(),default=str))
+
+result = RSW.call('Immunization',json.dumps(vaccine.dict(),default=str))
+if result is False:
+    print('Failed to call .... return values')
+else:
+    print (json.dumps(result.dict(), default=str))
--- rswfhir.py
+++ rswfhir.py
@@ -1,4 +1,10 @@
 import requests
+from jwt import JWT
+import base64
+from fhir.resources.R4B.bundle import Bundle
+from fhir.resources.R4B.operationoutcome import OperationOutcome
+from fhir.resources.R4B.operationoutcome import OperationOutcome
+from fhir.resources.R4B.immunization import Immunization
 
 class rswfhir:
     _url_base = 'https://accproxy.reseausantewallon.be/fhir/careset/'
@@ -11,4 +17,23 @@ class rswfhir:
     def call(self,request,data):
         request_url = self._url_base + request
         headers = {"Application_identifier": self._app_identifier,"Authorization": "Bearer " + self._bearer}
-        return requests.post(request_url, headers=headers, data=data, timeout=30)
+        if isinstance(data,str):
+            headers['Content-Type'] = 'application/json'
+        result = requests.post(request_url, headers=headers, data=data, timeout=30)
+        print (result)
+        result_json = result.json()
+        result_type = self._resource_type(result_json)
+        if result_type == False:
+            return False
+        if result_type == 'Bundle':
+            return Bundle(**result_json)
+        if result_type == 'OperationOutcome':
+            return OperationOutcome(**result_json)
+        if result_type == 'Immunization':
+            return Immunization(**result_json)
+        return False
+    
+    def _resource_type(self,response_json):
+        if 'resourceType' not in response_json.keys():
+            return False
+        return response_json['resourceType']
